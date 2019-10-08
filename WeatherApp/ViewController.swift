1f7eb14156609ce7d3c5c877d67c0ad099daeaa1
Joonas Salojärvi <c7jsaloj@kc408mac09.tamk.ad>
2019-10-08 16:09:41 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Joonas Salojärvi on 08/10/2019.
+//  Copyright © 2019 Joonas Salojärvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
