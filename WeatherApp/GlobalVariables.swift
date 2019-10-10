63abf5fa157c623b1c8263da2b1eee7bd08818f7
Joonas Salojärvi <c7jsaloj@kc408mac09.tamk.ad>
2019-10-10 10:44:59 +0300

    Global Variables added

---
+++ WeatherApp/GlobalVariables.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalVariables.swift
+//  WeatherApp
+//
+//  Created by Joonas Salojärvi on 10/10/2019.
+//  Copyright © 2019 Joonas Salojärvi. All rights reserved.
+//
+
+import Foundation
+class GlobalVariables {
+    var OWMkey : String = "API KEY HERE"
+}
